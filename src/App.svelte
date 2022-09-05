68fc07189121420a0b2ae13a4142b75e0f582c5a
crizmo <crizmo595@gmail.com>
2022-09-05 16:27:30 +0530

    ✨ : main route

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,8 @@
 <script>
-  import {Route} from 'tinro'; 
-
-  import Full from './routes/full.svelte';
-  import Beta from './routes/beta.svelte';
+  // import Full from './routes/full.svelte';
+  import Unit from './routes/unit.svelte';
 </script>
 
 <main>
-  <Route path="/">
-    <Full />
-	</Route>
-	
-	<Route path="/beta">
-		<Beta />
-	</Route>
+  <Unit />
 </main>
